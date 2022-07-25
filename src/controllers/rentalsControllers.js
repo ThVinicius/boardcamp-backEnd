53d3306c87e2c9c78376670f4fc921300c976f64
Vinicius Pacheco <vinicioss9955@gmail.com>
2022-07-24 22:20:37 -0300

    Fix: POST /rentals/:id/return

--- src/controllers/rentalsControllers.js
+++ src/controllers/rentalsControllers.js
@@ -1,19 +1,16 @@
 import connection from '../database/postgres.js'
-import dayjs from 'dayjs'
 
 export async function postRentals(req, res) {
   const { customerId, gameId, daysRented } = req.body
 
   const { pricePerDay } = res.locals
 
-  const now = dayjs().format('YYYY-MM-DD')
-
   try {
     const originalPrice = pricePerDay * daysRented
 
     await connection.query(
-      'INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee" ) VALUES ($1, $2, $3, $4, $5, $6, $7)',
-      [customerId, gameId, now, daysRented, null, originalPrice, null]
+      `INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee" ) VALUES ($1, $2, CURRENT_DATE, $3, $4, $5, $6)`,
+      [customerId, gameId, daysRented, null, originalPrice, null]
     )
 
     return res.sendStatus(201)
@@ -89,22 +86,16 @@ export async function getRentals(req, res) {
 }
 
 export async function returnRentals(_, res) {
-  const { rental } = res.locals
-
-  const now = dayjs().format('YYYY-MM-DD')
+  const { daysDiff, daysRented, pricePerDay, id } = res.locals.rental
 
   try {
-    const rentDate = dayjs(rental[0].rentDate).format('YYYY-MM-DD')
+    const calc = daysDiff - daysRented
 
-    const daysDiff = dayjs(now).diff(dayjs(rentDate), 'd')
-
-    const aux = rental[0].daysRented - daysDiff
-
-    const delayFee = aux < 0 ? Math.abs(aux) * rental[0].pricePerDay : 0
+    const delayFee = calc > 0 ? calc * pricePerDay : 0
 
     await connection.query(
-      'UPDATE rentals SET "returnDate" = $1, "delayFee" = $2',
-      [now, delayFee]
+      'UPDATE rentals SET "returnDate" = CURRENT_DATE, "delayFee" = $1 WHERE id = $2',
+      [delayFee, id]
     )
 
     return res.sendStatus(200)
--- src/middlewares/returnRentalsValidate.js
+++ src/middlewares/returnRentalsValidate.js
@@ -1,17 +1,25 @@
 import connection from '../database/postgres.js'
 
 export default async function returnRentalsValidate(req, res, next) {
-  const id = Number(req.params.id)
+  const { id } = req.params
 
   try {
     const { rows: rental } = await connection.query(
-      'SELECT r.*, g."pricePerDay" FROM rentals r JOIN games g ON r."gameId" = g.id WHERE r.id = $1',
+      `SELECT 
+        r.id, TO_CHAR(r."rentDate", 'YYYY-MM-DD') AS "rentDate", r."daysRented", 
+        (CURRENT_DATE - r."rentDate") AS daysDiff, r."returnDate",
+        g."pricePerDay" 
+        FROM rentals r 
+        JOIN games g ON r."gameId" = g.id 
+        WHERE r.id = $1 
+        LIMIT 1`,
       [id]
     )
 
-    if (rental.length === 0) return res.sendStatus(400)
+    if (rental.length === 0 || rental[0].returnDate !== null)
+      return res.sendStatus(400)
 
-    res.locals.rental = rental
+    res.locals.rental = rental[0]
 
     next()
   } catch (error) {
